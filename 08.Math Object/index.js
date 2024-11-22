b055a10689b6042e74acf3861d5f1bcd0fbb6c2f
8282 <ant.thanapa@gmail.com>
2024-11-22 21:53:37 +0700

    Create index.js

---
+++ 08.Math Object/index.js
@@ -0,0 +1,46 @@
+// math = built in object that provides a collection of properties and methods
+
+//example 
+
+console.log(Math.PI)
+
+let x = 81.6;
+let y = 2;
+let z = 3.00001;
+
+a = Math.round(x)
+console.log(a)
+//ทศนิยมมากกว่า.5ปัดขึ้น
+
+b = Math.floor(z)
+console.log(b)
+//ปัดทศนิยมลงเสมอ
+
+b = Math.ceil(z)
+console.log(b)
+//ปัดทศนิยมขึ้นเสมอ
+
+c = Math.trunc(z)
+console.log(c)
+//ตัดทศนิยมทิ้ง
+
+d = Math.pow(x , y)
+console.log(d)
+//ยกกำลัง x^y
+
+e = Math.sqrt(x)
+console.log(e)
+//รากที่2
+
+f = Math.log(x)
+console.log(f)
+//หาลอการิทึมธรรมชาติ(ฐาน6)
+
+// Math.sin
+// Math.cos
+// Math.tan
+
+
+g = Math.abs(x)
+console.log(g)
+//แปลงเป็น+เสมอ
