dde1ba3a53cf5a97ffdc3347c6d4e08c5928bcb0
8282 <ant.thanapa@gmail.com>
2024-11-23 01:58:18 +0700

    Update index.js

--- 12.Ternary Operator/index.js
+++ 12.Ternary Operator/index.js
@@ -4,6 +4,7 @@
 let age = 21
 
 let message = age >= 18 ? "dewasa" : "BocilXKematian"
+// ถ้าอายุ >= 18 จะเป็น "dewasa" ถ้าไม่ใช่จะเป็น "BocilXKematian"
 
 console.log(message);
 
@@ -11,4 +12,5 @@ console.log(message);
 
 let purchaseamount = 100;
 let diskon = purchaseamount >= 100? 10 : 0;
+// ถ้ายอดซื้อ >= 100 ได้ส่วนลด 10%, ถ้าน้อยกว่า 100 ได้ส่วนลด 0%
 console.log(`your total is $${purchaseamount - purchaseamount * (diskon/100)}`)
