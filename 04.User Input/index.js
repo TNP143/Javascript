10b317b187f328adff126eb8475ace3aef15ba45
8282 <ant.thanapa@gmail.com>
2024-11-22 18:23:05 +0700

    Update index.js

--- 04.User Input/index.js
+++ 04.User Input/index.js
@@ -16,5 +16,4 @@ let username;
 document.getElementById("mysubmit").onclick = function(){
     username = document.getElementById('mytext').value;
     document.getElementById("myh1").textContent = `hello ${username}`
-
 }
