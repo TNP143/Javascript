2fce15e5897c3ba290d53da2687a13db1e367116
8282 <ant.thanapa@gmail.com>
2024-11-22 23:14:08 +0700

    Update index.js

--- 09.Random Number Generator/index.js
+++ 09.Random Number Generator/index.js
@@ -1,22 +1,19 @@
 // // random generator
 
 // let ramdomNum1 = Math.random();
-
 // console.log(ramdomNum1)
+//สุ่มเลข0-1
 
 // let ramdomNum2 = Math.floor(Math.random() * 6) + 1;
-// // * 6 itu maksudnya batas maksimal angka yang di random adalah sampe < 6
-// // diluar ditambah + 1 baru menambahkan hasilnya ditambah menjadi x + 1
-
 // console.log(ramdomNum2)
+//สุ่มเลข0-5.99,ปัดทศนิยมลง,+1=เลื่อน0-5เป็น1-6
 
 // const min = 50;
 // const max = 100;
-
-// // let ramdomNum3 = Math.floor(Math.random() * max) + min; ini ga bisa membuat batas
 // let ramdomNum3 = Math.floor(Math.random() * (max - min)) + min;
-
 // console.log(ramdomNum3)
+//Math.random() * 50 =ได้เลขระหว่าง0-49.99
+//+ min = เลื่อนตัวเลขเข้าสู่ช่วงที่ต้องการ  
 
 const mybtn = document.getElementById("mybtn");
 const container = document.getElementById("container");
@@ -27,7 +24,6 @@ let randomNum4;
 
 mybtn.onclick = function() {
     randomNum4 = Math.floor(Math.random() * max1 ) + min1;
+    //สุ่มเขระหว่าง1-6
     container.textContent = randomNum4;
 }
-
-
