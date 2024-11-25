ecf52fe96b1c16b57c501b42120cd7191194d108
8282 <ant.thanapa@gmail.com>
2024-11-25 20:25:26 +0700

    Update index.js

--- 17.Logical Operators/index.js
+++ 17.Logical Operators/index.js
@@ -8,39 +8,25 @@
 // TRUE WITH TRUE IS TRUE
 // FALSE WITH FALSE IS TRUE
 // FALSE AND TRUE IS FALSE
-const temp = 20;
 
-if(temp > 0 && temp<= 30){
-    console.log("weather is freeze");
+//const temp = 20;
+//if(temp > 0 && temp<= 30){
+//    console.log("weather is freeze");
+//} else if(temp <= 20){
+//console.log("weather is good");
+//else {
+//    console.log("weather is bad");
+//}
 
-}
+//const temp1 = 200;
+//if(temp1 > 0 || temp1 <= 30){
+//    console.log("weather is freeze");
+//}
 // else if(temp <= 20){
 //     console.log("weather is good");
-// } //ini gausa dipake karna diatas udah di pake logika and
-else {
-    console.log("weather is bad");
-}
-
-// OR LOGICAL
-// TRUE WITH FALSE IS TRUE
-
-
-const temp1 = 200;
-
-if(temp1 > 0 || temp1 <= 30){
-    console.log("weather is freeze");
-
-}
-// else if(temp <= 20){
-//     console.log("weather is good");
-// } //ini gausa dipake karna diatas udah di pake logika and
-else {
-    console.log("weather is bad");
-}
-
-
-// NOT LOGICAL
-// CHANGE TRUE TO FALSE, AND FALSE TO TRUE
+//else {
+//    console.log("weather is bad");
+//}
 
 const isSunny = true;
 
